5c4ac57b22068933ab02bdb075fc08960c5340e0
Bart Spaans <onderstekop@gmail.com>
2009-08-06 06:22:20 +0200

    Installing man pages

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from distutils.core import setup
+import glob
+
 setup(name="btools",
       version="1.0rc4",
       description="Command line bookmarking tools",
@@ -8,7 +10,9 @@ setup(name="btools",
       packages = ["btools", "btools.matching"],
       scripts = ["scripts/bm", "scripts/mkbm", "scripts/bm-match"],
       data_files = [("/etc/bash_completion.d/", ["data/bm-completion"]),
-                    ("/usr/share/bm/", ["data/bm.bash", "data/bm-config-template"])],
+                    ("/usr/share/bm/", ["data/bm.bash", "data/bm-config-template"]),
+                    ("/usr/share/man/man1/", glob.glob("doc/man/*.1")),
+                   ]
 
      )
 
