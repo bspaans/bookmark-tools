2716c76cc297accb61f6cb8436e01e6c1d28dd05
Bart Spaans <onderstekop@gmail.com>
2009-08-21 12:55:33 +0200

    Added new fill-template tool

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import glob
 
 setup(name="btools",
-      version="0.9rc1",
+      version="0.99",
       description="Command line bookmarking tools",
       author="Bart Spaans",
       author_email = "onderstekop@gmail.com",
@@ -11,7 +11,7 @@ setup(name="btools",
       scripts = ["scripts/bm", "scripts/mkbm", "scripts/bm-match",
                  "scripts/bmsuggest", "scripts/bmsuggest-move", 
                  "scripts/bmsuggest-series", "scripts/bm-add-series", 
-                 "scripts/mvbm", "scripts/blog"],
+                 "scripts/mvbm", "scripts/blog", "scripts/fill-template"],
       data_files = [("/etc/bash_completion.d/", ["data/bm-completion"]),
                     ("/usr/share/bm/", ["data/bm.bash", "data/bm-config-template", "data/movies.patterns"]),
                     ("/usr/share/man/man1/", glob.glob("doc/man/*.1")),
