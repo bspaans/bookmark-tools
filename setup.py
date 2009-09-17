6179b6e09c16190f9301356883bb748db53b7141
Bart Spaans <onderstekop@gmail.com>
2009-09-17 04:55:14 +0200

    setup.py: Setting Path registry value if on Windows

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ else:
         import py2exe
     except: pass
 
+
     pf = os.environ["PROGRAMFILES"]
     scripts = [] # scripts are added to the 'bin' folder instead
     data = [(os.path.join(pf, "btools", "bm-match"), ["data/bm-match-config",
@@ -36,27 +37,67 @@ else:
 
 
 class source_shell_script(install_data):
-    def run(self):
-        install_data.run(self)
 
-        cmd = "source /usr/share/bm/bm.bash"
-
-        if path.exists("/etc/bash.bashrc") and path.exists("/usr/share/bm/bm.bash"):
-            f = open("/etc/bash.bashrc")
+    def source_bash_script(self, to="/etc/bash.bashrc"):
+        if path.exists(to) and path.exists("/usr/share/bm/bm.bash"):
+            f = open(to)
             bashrc = f.read()
             f.close()
 
+            cmd = "source /usr/share/bm/bm.bash"
             if cmd in bashrc.splitlines():
                 return
             else:
                 print "Sourcing bm.bash"
                 try:
-                    f = open("/etc/bash.bashrc", "a")
+                    f = open(to, "a")
                 except:
-                    print "Error: Couldn't write to /etc/bash.bashrc"
+                    print "Error: Couldn't write to %s" % to
                     return
                 f.write(cmd + os.linesep)
                 f.close()
+        else:
+            print "Warning: the shell functions could not be sourced"
+
+    def update_windows_registry(self):
+        try:
+            import _winreg
+        except:
+            print "Couldn't change PATH variable in registry, because the _winreg modules could not be loaded."
+            return
+
+        path = os.path.join(os.environ["PROGRAMFILES"], "btools", "bin")
+        regpath = r'SYSTEM\CurrentControlSet\Control\Session Manager\Environment'
+        reg = _winreg.ConnectRegistry(None, _winreg.HKEY_LOCAL_MACHINE)
+
+        try:
+            key = _winreg.OpenKey(reg, regpath, 0, _winreg.KEY_ALL_ACCESS)
+        except:
+            print "Error getting PATH from registry"
+            return
+
+        value, type_id = _winreg.QueryValueEx(key, "PATH")
+
+        current = value.split(";")
+        if path in current:
+            return
+
+        try: 
+            _winreg.SetValueEx(key, "PATH", 0, type_id, ";".join(current + [path]))
+            print "Updated %s\\PATH registry value" % (regpath)
+        except:
+            print "Error updating %s\\PATH value" % (regpath)
+
+
+
+    def run(self):
+        install_data.run(self)
+
+        if not WINDOWS:
+            self.source_bash_script()
+        else:
+            self.update_windows_registry()
+
 
 
 
